29492622b6a804cdc6f648b82d255ef2b60098f4
Gowrishankar M N <142252255+gowrishankar-14@users.noreply.github.com>
2024-08-14 11:44:52 +0530

    LinkedList In Java

---
+++ LinkList.java
@@ -0,0 +1,67 @@
+class LinkedList
+{
+    Node head;
+    class Node
+    {
+    int data;
+    Node next;
+    
+    Node(int val)
+    {
+        data=val;
+        next=null;
+    }
+    }
+    
+    LinkedList()
+    {
+        head=null;
+    }
+    
+    public void insert1st(int val)
+    {
+        Node new_node=new Node(val);
+        
+        if(head==null)
+        {
+            head=new_node;
+        }
+        else
+        {
+            new_node.next=head;
+            head=new_node;
+        }
+    }
+    
+    public void display()
+    {
+        Node current=head;
+        
+        while(current!=null)
+        {
+            System.out.print(current.data+"->");
+            current=current.next;
+        }
+        
+        System.out.println("Null");
+    }
+}
+
+
+public class LinkList
+{
+    public static void main(String[] args)
+    {
+        LinkedList ll=new LinkedList();
+        
+        ll.insert1st(10);
+        ll.insert1st(20);
+        ll.insert1st(30);
+        ll.insert1st(40);
+
+        ll.display();
+    }
+}
+
+
+
